042a8679bb9e1a47a28d9729fe0ad13bcde76b8d
Quentin <q.barthelemy@student.helmo.be>
2023-03-05 02:03:59 +0100

    added red color fade on swipe delete (chipoterie)

--- app/src/main/java/be/helmo/myscout/view/meeting/meetinglist/MeetingListFragment.kt
+++ app/src/main/java/be/helmo/myscout/view/meeting/meetinglist/MeetingListFragment.kt
@@ -1,11 +1,14 @@
 package be.helmo.myscout.view.meeting.meetinglist
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +18,7 @@ import be.helmo.myscout.factory.PresenterSingletonFactory
 import be.helmo.myscout.factory.interfaces.IMeetingPresenterCallback
 import be.helmo.myscout.view.interfaces.IMeetingListPresenter
 import java.util.*
+import kotlin.math.absoluteValue
 
 class MeetingListFragment : Fragment(), IMeetingPresenterCallback {
     var recyclerView: RecyclerView? = null
@@ -76,12 +80,42 @@ class MeetingListFragment : Fragment(), IMeetingPresenterCallback {
 
                         // Change the background color of the item view to red when swiped left
                         if (dX < 0) {
+                            val background = ColorDrawable(ContextCompat.getColor(itemView.context, R.color.red))
+                            val swipeWidthPercentage = (dX.absoluteValue * 2 / itemView.width.toFloat()).coerceIn(0f, 1f)
+
+                            ValueAnimator.ofInt(0, (255 * swipeWidthPercentage).toInt()).apply {
+                                duration = 0
+                                addUpdateListener { animator ->
+                                    val alpha = animator.animatedValue as Int
+                                    background.alpha = alpha
+                                    itemView.setBackgroundColor(background.color)
+                                }
+                                start()
+                            }
+
+                            // Draw the background color and icon
+                            background.setBounds(
+                                itemView.right + dX.toInt(),
+                                itemView.top,
+                                itemView.right,
+                                itemView.bottom
+                            )
+                            background.draw(c)
+
                             //itemView.setBackgroundResource(R.color.light_red)
-                            itemView.setBackgroundColor(Color.RED)
+
+                            /*
+                            itemView.animate()
+                                .setDuration(0)
+                                .alpha(swipeWidthPercentage)
+                                .withEndAction { itemView.alpha = swipeWidthPercentage }
+                                .start()
+
+                             */
                         } else {
                             itemView.setBackgroundResource(R.color.transparent)
-                            itemView.setBackgroundColor(Color.TRANSPARENT)
                         }
+
                         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
                     }
                 }
@@ -98,26 +132,6 @@ class MeetingListFragment : Fragment(), IMeetingPresenterCallback {
         Log.d(TAG, "onViewCreated called")
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume called")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart called")
-    }
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
         Log.d(TAG, "onAttach called")
@@ -130,14 +144,10 @@ class MeetingListFragment : Fragment(), IMeetingPresenterCallback {
         //callback = null
     }
 
-    override fun onMeetingDataAdd(meetingIndex: Int) { //todo enlever list ?
-        // Update the RecyclerView with the new data
+    override fun onMeetingDataAdd(meetingIndex: Int) {
         requireActivity().runOnUiThread {
             recyclerView?.adapter?.notifyItemChanged(meetingIndex)
-            //recyclerView?.adapter?.
         }
-        //recyclerView?.adapter?.notifyDataSetChanged()
-        //recyclerView?.adapter.updateData(meetingViewModels)
     }
 
     companion object {
